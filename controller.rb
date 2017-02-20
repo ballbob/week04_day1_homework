c74f0a934d73abe3329b8367c79b734577110518
Robert Ball <robert.ball.1989@gmail.com>
2017-02-20 22:23:37 +0000

    CSS and ERB files function, however making an overall 'play' class for outcomes has not. Will go over this tomorrow.

--- controller.rb
+++ controller.rb
@@ -3,3 +3,13 @@ require 'sinatra/contrib/all' if development?
 require 'pry'
 require './models/rockpaperscissors.rb'
 require 'json'
+
+get '/play/:player1/:player2' do
+  game = RockPaperScissors.new(params[:player1].to_s, params[:player2].to_s)
+  @game = game.play(params[:player1].to_s, params[:player2].to_s)
+  erb ( :outcome )
+end
+
+get '/' do
+  erb (:home)
+end
--- models/rockpaperscissors.rb
+++ models/rockpaperscissors.rb
@@ -15,7 +15,7 @@ class RockPaperScissors
     end
   end
 
-  def paper
+  def paper 
     if @player2 == "paper"
       return "Draw."
     elsif @player2 == "scissors"
@@ -26,6 +26,30 @@ class RockPaperScissors
   def scissors
     if @player2 == "scissors"
       return "Draw."
+    end
+  end
+
+  def play
+    if @player1 == "rock" 
+      if @player2 == "scissors"
+        return "Player 1 wins"
+      elsif @player2 == "paper"
+        return "Player 2 wins."
+      end
+    elsif @player1 == "paper"
+      if @player2 == "rock"
+        return "Player 1 wins."
+      elsif @player2 = "scissors"
+        return "Player 2 wins."
+      end
+    elsif @player1 == @player2
+      return "Draw."
+    elsif @player1 == "scissors"
+      if @player2 == "rock"
+        return "Player 2 wins."
+      elsif @player2 == "paper"
+        return "Player 1 wins."
+      end
     end
   end
 
